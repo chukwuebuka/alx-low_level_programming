5aab37e0ccc9bc98bb41f44aa829e51067097a8f
chukwuebuka <valchukzy@gmail.com>
2023-03-27 10:52:44 -0700

    fff

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet 10 times
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	
+	for(i = 0,i <10, i++)
+	{
+		char c = 'a';
+
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+
+		_putchar('\n');
+	}
+}
